4320d82cfbc11e2cce999743e62e9a13d2322504
s0ojin <onepst@hanyang.ac.kr>
2022-05-21 02:31:32 +0900

    add sign-up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+
+
+const theme = {
+  bgColor: "#dfe6e9",
+  textColor: "#2d3436",
+  accentColor: "#0984e3"
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+      <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
