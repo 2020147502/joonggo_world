1ed874bfbca5b5c1d4b142810c03084ecd957839
s0ojin <onepst@hanyang.ac.kr>
2022-06-24 15:17:24 +0900

    fix signup

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,7 +1,10 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { fetchSignUp } from "../api";
+
+
 
 const Input = styled.input`
   width: 100%;
@@ -24,7 +27,6 @@ const Input = styled.input`
 const Container = styled.div`
   max-width: 500px;
   margin: 0px auto;
-
   h1 {
     text-transform: uppercase;
     font-size: 50px;
@@ -49,82 +51,68 @@ const SubmitBtn = styled.button`
   margin-bottom: 50px;
 `;
 
-const Label = styled.button`
-  margin: 30px;
-`;
-
 function SignUp() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm();
+  const { register, handleSubmit, formState: {errors}, setError } = useForm();
   const mutation = useMutation(fetchSignUp);
+  const navigate = useNavigate();
   const onValid = (data) => {
-    if (data.password !== data.confirmPassword) {
-      setError(
-        "confirmPassword",
-        { message: "비밀번호가 일치하지 않습니다." },
-        { shouldFocus: true }
-      );
+    if(data.password !== data.confirmPassword) {
+      setError("confirmPassword",
+      {message:"비밀번호가 일치하지 않습니다."},
+      {shouldFocus: true})
     }
-    mutation.mutate(data);
-  };
+    else(  
+      mutation
+      .mutateAsync(data)
+      .then((res) => {      
+        if(res.success) {
+        alert("회원가입이 완료되었습니다.")
+        navigate("/login")
+        }
+        if(res.err?.code === 11000) {
+          alert("이미 사용중인 이메일 입니다.")
+        }
+      }))
+  }
 
-  return (
+  return(
     <Container>
       <h1>sign up</h1>
       <form onSubmit={handleSubmit(onValid)}>
-        <label htmlFor="email">이메일</label>
-        <Input
-          id="email"
-          {...register("email", {
-            required: "이메일은 필수값입니다.",
-            pattern: {
-              value:
-                /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
-              message: "이메일 양식에 맞지 않습니다.",
-            },
+        <label htmlFor="email">Email</label>
+        <Input id="email" {...register("email", {
+          required:"이메일은 필수값입니다.",
+          pattern: {
+            value: /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+            message:"이메일 양식에 맞지 않습니다."
+          }
           })}
         />
         <span>{errors?.email?.message}</span>
-        <label htmlFor="username">이름</label>
-        <Input
-          id="username"
-          {...register("username", {
-            required: "사용자이름은 필수값입니다.",
-            minLength: {
-              value: 2,
-              message: "사용자이름은 2글자 이상입니다.",
-            },
-          })}
-        />
+        <label htmlFor="username">Username</label>
+        <Input id="username" {...register("username", {
+          required:"사용자이름은 필수값입니다.", 
+          minLength: {
+            value:2,
+            message:"사용자이름은 2글자 이상입니다."
+        }})} />
         <span>{errors?.username?.message}</span>
-        <label htmlFor="password">비밀번호</label>
-        <Input
-          id="password"
-          {...register("password", {
-            required: "비밀번호는 필수값입니다.",
-            minLength: {
-              value: 8,
-              message: "비밀번호는 8글자 이상입니다.",
-            },
-          })}
-        />
+        <label htmlFor="password">Password</label>
+        <Input id="password" {...register("password",{
+          required:"비밀번호는 필수값입니다.",
+          minLength: {
+            value:8,
+            message:"비밀번호는 8글자 이상입니다."
+          }
+        })} />
         <span>{errors?.password?.message}</span>
-        <label htmlFor="confirmPassword">비밀번호 재확인</label>
-        <Input
-          id="confirmPassword"
-          {...register("confirmPassword", {
-            required: "비밀번호 확인은 필수값입니다.",
-          })}
-        />
+        <label htmlFor="confirmPassword">Confirm password</label>
+        <Input id="confirmPassword" {...register("confirmPassword", {required:"비밀번호 확인은 필수값입니다."})} />
         <span>{errors?.confirmPassword?.message}</span>
-        <SubmitBtn>가입하기</SubmitBtn>
+        <SubmitBtn>sign up</SubmitBtn>
       </form>
     </Container>
-  );
+  )
 }
 
 export default SignUp;
