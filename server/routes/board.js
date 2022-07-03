60c5727363c2b6aadb9f932534459d101bbad349
funlol@naver.com <chang525>
2022-07-03 22:32:35 +0900

    multer

--- server/routes/board.js
+++ server/routes/board.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { Board } = require("../models/Board");
-
+const multer = require('multer')
 // ----------------------게시판------------------------
 
 // 상품 등록하기
@@ -96,4 +96,27 @@ router.get("/products_by_id", (req, res) => {
         return res.status(200).send(product);
       });
   });
+
+//이미지 등록
+var storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function ( req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now())
+    }
+})
+
+var upload = multer({storage : storage}).single("file")
+
+router.post('/image', (req,res) => {
+    upload(req,res,err => {
+        upload(req,res,err => {
+            if (err) {
+                return req,json({ success: false,err})
+            }
+            return res.json({sucess:true, filePath : res.req.file.path, fileName: res.req.file.filename})
+        })
+    })
+})
 module.exports = router;
